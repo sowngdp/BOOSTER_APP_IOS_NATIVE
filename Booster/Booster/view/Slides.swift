7cd7c32fed4fc6a8c08b8621128d445c5c41e142
Fy Spoti <>
2023-10-02 17:35:40 +0700

    mmmm

---
+++ Booster/Booster/view/Slides.swift
@@ -0,0 +1,22 @@
+//
+//  Slides.swift
+//  Booster
+//
+//  Created by Fy Spoti on 02/10/2023.
+//
+
+import UIKit
+
+class Slides: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
