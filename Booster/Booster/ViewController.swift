298e4c859833fa84f016f8b8115dfd14f3cf70fb
Fy Spoti <>
2023-10-02 13:56:13 +0700

    first commit

---
+++ Booster/Booster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Booster
+//
+//  Created by Fy Spoti on 02/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
