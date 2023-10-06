bcf395eca33f3c3e3b5491eb623bb5d03fadcdc2
Fy Spoti <>
2023-10-06 11:03:37 +0700

    fix

--- Booster/Booster/LoginControllerViewController.swift
+++ Booster/Booster/LoginControllerViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class LoginControllerViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var PWTF: UITextField!
+    @IBOutlet weak var Hide_or_Unhide_pw: UIImageView!
+    var iconClick = true
     override func viewDidLoad() {
         super.viewDidLoad()
+        //PWTF.isSecureTextEntry = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        Hide_or_Unhide_pw.isUserInteractionEnabled = true
+        Hide_or_Unhide_pw.addGestureRecognizer(tapGestureRecognizer)
 
         // Do any additional setup after loading the view.
     }
     
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        if iconClick == true {
+            PWTF.isSecureTextEntry = false
+            iconClick = false
+        }else{
+            PWTF.isSecureTextEntry = true
+            iconClick = true
+        }
+        
+    }
+
 
     /*
     // MARK: - Navigation
