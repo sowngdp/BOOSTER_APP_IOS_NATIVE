8a42ddaba2b5f340ab6eb477630dccf16d6d4514
Fy Spoti <>
2023-10-27 08:04:50 +0700

    .

---
+++ Booster/Booster/Models/ObjectModel/UserInputData.swift
@@ -0,0 +1,13 @@
+//
+//  UserInputData.swift
+//  Booster
+//
+//  Created by Fy Spoti on 26/10/2023.
+//
+
+import Foundation
+
+struct UserInputData {
+    var email: String
+    var password: String
+}
