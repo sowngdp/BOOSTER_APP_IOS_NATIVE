3274158c5fe11046cd80c70344e1e0b2c62d3116
frenkysown <sownnickey@gmail.com>
2023-10-27 10:26:13 +0700

    ...

--- Booster/Booster/Delegates/AppDelegate.swift
+++ Booster/Booster/Delegates/AppDelegate.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseAuth
+import FirebaseStorage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
         return true
     }
 
