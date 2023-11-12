dc74cb9f1615d03c2e2036890ae1470497f24251
frenkysown <sownnickey@gmail.com>
2023-11-13 03:59:40 +0700

    .....

--- Booster/Booster/Controllers/Signup_VC.swift
+++ Booster/Booster/Controllers/Signup_VC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class Signup_VC: UIViewController {
-
+    
     @IBOutlet weak var emailUser: UITextField!
     @IBOutlet weak var nameUser: UITextField!
     @IBOutlet weak var passwordUser: UITextField!
@@ -17,20 +17,20 @@ class Signup_VC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @IBAction func signUpButton(_ sender: Any) {
         
         var user : UserModel = UserModel()
@@ -41,11 +41,18 @@ class Signup_VC: UIViewController {
         AuthViewModel.share.signup(withUser: user){
             Result in
             
-            print(Result)
-            
+            switch Result {
+            case .success:
+                // Đăng ký thành công, gọi hàm setTabBarRootView
+                if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
+                    sceneDelegate.setRootViewTabbar()
+                }
+            case .failure(let error):
+                // Xử lý lỗi đăng ký (nếu cần)
+                print("Failed to signup: \(error.localizedDescription)")
+            }
         }
         
-        performSegue(withIdentifier: "signUpToTabbarView", sender: nil)
     }
     
 }
---
+++ Booster/Booster/view/CollectionItemGame.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionItemGame.swift
+//  Booster
+//
+//  Created by hoi on 12/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class CollectionItemGame: UICollectionViewCell {
+    
+}
